96cb8c6f83f312173e0405c1576714e689b30dcb
edna <edna@LAPTOP-9PK5LSTL>
2022-12-05 14:27:30 +0100

    add router base

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <h1>This is Header</h1>
-        <NuxtLink to="/">Home page</NuxtLink>
-        <NuxtLink to="/about">About page</NuxtLink>
-        <NuxtLink to="/contact">Contact page</NuxtLink>
+        <NuxtLink to="{{ router.base }} /">Home page</NuxtLink>
+        <NuxtLink to="{{ router.base }} /about">About page</NuxtLink>
+        <NuxtLink to="{{ router.base }} /contact">Contact page</NuxtLink>
     </div>
 </template>
 
