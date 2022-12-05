02c8f2813ad097fa2c43ba6a294076ee4f22a66c
edna <edna@LAPTOP-9PK5LSTL>
2022-12-05 14:07:21 +0100

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+import { Base } from './.nuxt/components.d';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-export default defineNuxtConfig({
+export default ({
+    target: 'static',
+    router: {
+      base:'/memomd/',
+    },
     vite: {
         css: {
           preprocessorOptions: {
@@ -34,3 +39,4 @@ export default defineNuxtConfig({
       }    
     }
 })
+
