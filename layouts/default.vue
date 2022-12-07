3fc9b03c64055e066125fc772cc18f77624301f1
edna <edna@LAPTOP-9PK5LSTL>
2022-12-07 13:46:22 +0100

    adding loginpage

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,13 @@
     <header>
       <h1>Heading</h1>
     </header>
-    <!--this is the page content-->
+    <!--underneath is the page content-->
     <div>
       <slot/>
     </div>
+    <footer>
+      <button>Cancel</button>
+    </footer>
   </div>
 </template>
 
@@ -15,10 +18,34 @@
 </script>
 
 <style scoped>
-h1 {
-  color: var(--heading-color);
-  font-weight: var(--bold-text);
-  font-family: var(--fonts)
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
 }
+html, body{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+header{
+  margin-top: 100px;
+}
+h1 {
+  color: var(--text-color);
+  font-weight: var(--bold-text);
+  font-family: var(--fonts);
+  text-align: center;
+}
+footer{
+  text-align: center;
+}
+button{
+  background-color: var(--cta-bg-color);
+  border: none;
+  padding: 10px;
+  width: 350px;
+}
+
 
 </style>
--- pages/loginpage.vue
+++ pages/loginpage.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="loginpage">
+    <button class="email">Email</button>
+    <button class="adgangskode">Adgangskode</button>
+    <button>Log ind</button>
+    <div class="rememberme">
+      <!-- <button class="btn-rememberme">Husk Mig</button>
+      <p>Jeg har glemt min adgangskode</p> -->
+      <input type="checkbox" id="checkbox" v-model="checked" value="Husk Mig"/>
+	    <label for="checkbox">{{ checked }}</label>
+    </div> 
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const checked = ref(true)
 </script>
 
 <style scoped>
-
+.loginpage{
+  flex: 1 0 auto;
+  padding: 2rem;
+  width: 350px;
+  margin: 0 auto;
+}
+.email, .adgangskode {
+  background-color: var(--secondary-color);
+  border: none;
+  margin: 10px;
+  padding: 10px;
+  width: 350px;
+}
+button{
+  background-color: var(--cta-bg-color);
+  border: none;
+  padding: 10px;
+  width: 350px;
+  margin: 10px;
+}
+.rememberme{
+  text-align: left;
+}
 </style>
