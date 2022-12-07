4634ee3c4dd6009b85fdf0b1aaed353d3b2fe552
edna <edna@LAPTOP-9PK5LSTL>
2022-12-07 10:40:37 +0100

    startpage lavede

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,7 @@
 <template>
   <div>
     <header>
-      <nav>
-        <NuxtLink to="/">This is the logo</NuxtLink>
-        <ul>
-          <li><NuxtLink to="/">Home page</NuxtLink></li>
-          <li><NuxtLink to="/profilepage/">Profile</NuxtLink></li>
-          <li><NuxtLink to="/settingspage/">Settings</NuxtLink></li>
-          <li><NuxtLink to="/flashcards/">Overview</NuxtLink></li>
-          <li><NuxtLink to="/flashcards/questionpage">Question page</NuxtLink></li>     
-        </ul>
-      </nav>
+      <h1>Heading</h1>
     </header>
     <!--this is the page content-->
     <div>
@@ -19,11 +10,13 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+
+</script>
 
 <style scoped>
-.router-link-exact-active {
-  color: var(--router-active-color);
+h1 {
+  color: var(--heading-color);
   font-weight: var(--bold-text);
   font-family: var(--fonts)
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,64 @@
 <template>
-  <div>
-   <img src="~/assets/images/logo.png" alt="this is the memomd logo"/>
-  <h1>Using Default layout</h1>
-    <p>
-      Nuxt is an open-source framework under MIT license for building modern and performant web applications that can be deployed on any platform running JavaScript.
-    </p>
+  <div class="startpage">
+    <section class="startsection">
+      <section class="section1">
+        <img 
+        class="logo" 
+        src="~/assets/images/logo.png" 
+        alt="this is the memomd logo" 
+        />
+      </section>
+      <section class="section2">
+        <button>Login</button>
+        <button>Opret Bruger</button>
+      </section>
+    </section>
   </div>
 </template>
 
 <script setup>
-// This will work in both `<script setup>` and `<script>`
-// definePageMeta({
-//   layout: "false",
-// });
+//This will work in both `<script setup>` and `<script>`
+definePageMeta({
+  layout: "false",
+});
 </script>
 
 <style scoped>
-h1 {
-  background-color: var(--bg-color);
-  color: var(--heading-color);
-  padding: 100px;
+.startpage {
+  position: fixed;
+  width: 100%;
+  min-width: 280px;
 }
-p{
-  color: var(--text-color),
+.startsection {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10px;
+  position: relative;
+  margin: auto;
+  top: 100px;
+  right: 0;
+  width: 480px;
+}
+.section1 {
+  text-align: center;
+  width: 480px;
+}
+.logo{
+  background-color: var(--primary-color); 
+  padding: 100px 100px;
+}
+.section2 {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+}
+button{
+  background-color: var(--primary-color);
+  border: none;
+  padding: 10px;
+  margin: 40px 0px 0px 64px; 
+  width: 350px;
 }
 </style>
