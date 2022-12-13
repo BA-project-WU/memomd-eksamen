bbfba7747a42c308ab58931cb41f2190d892b7ec
edna <edna@LAPTOP-9PK5LSTL>
2022-12-13 15:54:19 +0100

    adding settings page

---
+++ pages/reportpage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Rapport et Problem</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/settingspage.vue
+++ pages/settingspage.vue
@@ -1,13 +1,185 @@
 <template>
-    <div>
-        <h1>This is Settings Page </h1>
-    </div>
+  <div>
+    <TheFlashcardHeader
+      flashcard-heading="Instillinger"
+    ></TheFlashcardHeader>
+      <div class="settings-box">
+        <div class="lyd">
+          <h4>Lyfeffekter</h4>
+          <input type="checkbox" id="switch" /><label for="switch">Lydeefekter</label>
+        </div>
+        <div class="lyd">
+          <h4>Notifikationer</h4>
+          <input type="checkbox" id="switch" /><label for="switch">Notifikationer</label>
+        </div>
+        <div class="policy-and-report">
+          <NuxtLink class="policy" to="/policypage">Privatlivspolitik</NuxtLink>
+          <NuxtLink class="report" to="/reportpage">Rapporter et problem</NuxtLink>
+        </div>
+        <div class="logud">
+          <NuxtLink class="logud-btn" to="/" @click="logud">Logud</NuxtLink>
+        </div>
+      </div>
+      <footer>
+      <div class="start-the-game">
+        <button>Start</button>
+      </div>
+      <ul class="footer-list">
+        <li><NuxtLink to="/settingspage">Settings</NuxtLink></li>
+        <li><NuxtLink to="/profilepage">Profile</NuxtLink></li>
+      </ul>
+    </footer>
+  </div>
 </template>
 
 <script setup>
-
+definePageMeta({
+  layout: "false",
+});
 </script>
 
 <style scoped>
+body {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100vh;
+}
+.heading-overview {
+  background: rgb(168, 228, 192);
+  background: radial-gradient(
+    circle,
+    rgba(168, 228, 192, 1) 35%,
+    rgba(78, 210, 202, 1) 100%
+  );
+  border-bottom-left-radius: 25px;
+  border-bottom-right-radius: 25px;
+  margin-top: -18px;
+  padding: 50px 50px;
+  text-align: center;
+}
+.settings-box {
+  background: var(--secondary-color);
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  position: absolute;
+  width: 89%;
+  top: 140px;
+  left: 20px;
+}
+.lyd {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 10px;
+}
+.policy-and-report{
+  display: flex;
+  justify-content: space-evenly;
+}
 
+h4{
+  margin-right: 10px;
+}
+input{
+  height: 0;
+  width: 0;
+  visibility: hidden;
+}
+label {
+  cursor: pointer;
+  text-indent: -9999px;
+  width: 40px;
+  height: 20px;
+  background: grey;
+  display: block;
+  border-radius: 20px;
+  position: relative;
+}
+label:after {
+  content: '';
+  position: absolute;
+  top: 3px;
+  left: 3px;
+  width: 15px;
+  height: 14px;
+  background: #fff;
+  border-radius: 10px;
+  transition: 0.3s;
+}
+
+input:checked + label {
+  background: var(--primary-color);
+}
+
+input:checked + label:after {
+  left: calc(100% - 5px);
+  transform: translateX(-100%);
+}
+
+label:active:after {
+  width: 10px;
+}
+a{
+  text-decoration: none;
+  color: var(--text-color);
+}
+.logud{
+  text-align: center;
+  position: relative;
+  top: 35px;
+}
+.logud-btn{
+  padding: 10px 50px;
+  background-color: var(--primary-color);
+  box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%), 0 6px 20px 0 rgb(0 0 0 / 19%);
+}
+
+footer {
+  background: rgb(168, 228, 192);
+  background: radial-gradient(
+    circle,
+    rgba(168, 228, 192, 1) 35%,
+    rgba(78, 210, 202, 1) 100%
+  );
+  bottom: 0; /* Height of the footer */
+  border-top-left-radius: 25px;
+  border-top-right-radius: 25px;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  margin: 0 auto;
+  min-width: 280px;
+  width: 100%;
+}
+.footer-list {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  text-decoration: none;
+  list-style-type: none;
+  position: relative;
+  top: -20px;
+}
+li {
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+a {
+  color: var(--text-color);
+  text-decoration: none;
+}
+.start-the-game{
+  text-align: center;
+  position: relative;
+  top: -18px;
+}
+button{
+  padding: 10px 50px;
+  border-radius: 20px;
+  background-color: var(--primary-color);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
 </style>
