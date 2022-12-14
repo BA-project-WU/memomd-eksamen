0f0d71b3c3549831b46ce45e01d9a35c89882c28
edna <edna@LAPTOP-9PK5LSTL>
2022-12-15 00:02:53 +0100

    up

--- pages/reportpage.vue
+++ pages/reportpage.vue
@@ -3,17 +3,31 @@
     <TheFlashcardHeader flashcard-heading="Reporter Et Problem"></TheFlashcardHeader>
     <div class="settings-box">
       <form @submit.prevent="sendReport">
-        <form @submit.prevent="sendReport">
-                <input type="text" name="title-input" placeholder="Report Titlen" v-model="reportTitle" />
-                <select name="reportOptions" v-model="reportOptions">
-                    <option value="Hvad drejer dit problem sig om?">Hvad drejer dit problem sig om?</option>
-                    <option value="Spørgsmål/svar: Forkert rigtigt svar">Spørgsmål/svar: Forkert rigtigt svar</option>
-                    <option value="Spørgsmål/svar: Formulering af spørgsmål og/eller svar">Spørgsmål/svar: Formulering af spørgsmål og/eller svar</option>
-                    <option value="Andet">Andet</option>
-                </select>
-                <textarea name="reportMessage" v-model="reportMessage" rows="8" cols="50"></textarea>
-                <input placeholder="Send Report" type="submit" v-on:click="sendReport()" />
-            </form>
+        <input
+          type="text"
+          name="title"
+          placeholder="Report Titlen"
+          v-model="reportTitle"
+        />
+        <select name="reportOptions" v-model="reportOptions">
+          <option value="Hvad drejer dit problem sig om?">
+            Hvad drejer dit problem sig om?
+          </option>
+          <option value="Spørgsmål/svar: Forkert rigtigt svar">
+            Spørgsmål/svar: Forkert rigtigt svar
+          </option>
+          <option value="Spørgsmål/svar: Formulering af spørgsmål og/eller svar">
+            Spørgsmål/svar: Formulering af spørgsmål og/eller svar
+          </option>
+          <option value="Andet">Andet</option>
+        </select>
+        <textarea
+          name="reportMessage"
+          v-model="reportMessage"
+          rows="8"
+          cols="50"
+        ></textarea>
+        <input placeholder="Send Report" type="submit" v-on:click="sendReport()" />
       </form>
     </div>
     <footer>
@@ -40,7 +54,7 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import axios from "axios";
 const { umbracoProjectAlias } = useRuntimeConfig();
 const { umbracoApiKey } = useRuntimeConfig();
@@ -49,21 +63,35 @@ definePageMeta({
   layout: "false",
 });
 
-function sendReport() {
-  // Send a POST request
-  axios.post('https://api.umbraco.io/forms/e644a504-a515-4974-ac9a-b8a9be734edc/entries', {
-    data: {
-      "reportTitle": this.reportTitle,
-      "reportOptions": [this.reportOptions],
-      "reportMessage": this.reportMessage
-    },   
-      headers: {
-        "umb-project-alias": umbracoProjectAlias,
-        "api-key": umbracoApiKey,
-      },
+export default {
+  data() {
+    return {
+      reportTitle: "",
+      reportOptions: "",
+      reportMessage: "",
+    };
+  },
+  methods: {
+    async sendReport() {
+      axios.post(
+        "https://api.umbraco.io/forms/e644a504-a515-4974-ac9a-b8a9be734edc/entries",
+        {
+          reportTitle: this.reportTitle,
+          reportOptions: [this.reportOptions],
+          reportMessage: this.reportMessage,
+        },
+        {
+          headers: {
+            "umb-project-alias": umbracoProjectAlias,
+            "api-key": umbracoApiKey,
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
     },
-  );
-}
+  },
+};
 </script>
 
 <style scoped>
