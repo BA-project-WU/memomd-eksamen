38f9bb34bbe9d87b7b2073be402c9c70dcf7b4ec
amal1698 <amal1698@edu.ucl.dk>
2022-12-19 15:18:52 +0100

    100%

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -65,7 +65,7 @@ export default defineNuxtConfig({
   },
   auth: {
     isEnabled: true,
-    origin: "http://10.240.11.157:3000",
+    origin: "http://10.228.110.93:3000",
     basePath: "/api/auth",
     enableSessionRefreshPeriodically: false,
     enableSessionRefreshOnWindowFocus: true,
