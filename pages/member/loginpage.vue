3a14233651998f4680b0c0f038c15fdf9e4f2c89
Amalie Lauritsen <42379939+AmalieLauritsen@users.noreply.github.com>
2022-12-22 15:13:18 +0100

    Update loginpage.vue

--- pages/member/loginpage.vue
+++ pages/member/loginpage.vue
@@ -4,7 +4,7 @@
 <script setup>
 const { signIn } = useSession()
 // await signIn()
-await signIn(undefined, { callbackUrl: '/flashcards/' })
+await signIn(undefined, { callbackUrl: 'https://ba-project-wu-memomd.netlify.app/flashcards' })
 
 definePageMeta({
     auth: false
