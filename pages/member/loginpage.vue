9fd2d90a5c0729cddb0f880a481f5601c30b4bd4
edna <edna@LAPTOP-9PK5LSTL>
2022-12-22 15:34:06 +0100

    e

--- pages/member/loginpage.vue
+++ pages/member/loginpage.vue
@@ -4,7 +4,7 @@
 <script setup>
 const { signIn } = useSession()
 // await signIn()
-await signIn(undefined, { callbackUrl: 'https://ba-project-wu-memomd.netlify.app/flashcards' })
+await signIn("Credentials", { callbackUrl: 'https://ba-project-wu-memomd.netlify.app/flashcards' })
 
 definePageMeta({
     auth: false
