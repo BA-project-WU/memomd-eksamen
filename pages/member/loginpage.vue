80258ad3098b12896497ba51e3ab676e51693346
Amalie Lauritsen <42379939+AmalieLauritsen@users.noreply.github.com>
2022-12-22 15:54:32 +0100

    Update loginpage.vue

--- pages/member/loginpage.vue
+++ pages/member/loginpage.vue
@@ -4,7 +4,8 @@
 <script setup>
 const { signIn } = useSession()
 // await signIn()
-await signIn("Credentials", { callbackUrl: '/flashcards/' })
+// await signIn("Credentials", { callbackUrl: '/flashcards/' })
+await signIn("undefined", { callbackUrl: '/flashcards/' })
 
 
 definePageMeta({
