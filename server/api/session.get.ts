3eb20ba1692818596c545a621bddf466f8259c94
edna <edna@LAPTOP-9PK5LSTL>
2022-12-22 16:03:34 +0100

    d

--- server/api/session.get.ts
+++ server/api/session.get.ts
@@ -1,7 +1,6 @@
 import { getServerSession } from "#auth";
 
 export default eventHandler(async (event) => {
-  console.log('test1')
   const session = await getServerSession(event);
   if (!session) {
     return { status: "unauthenticated!" };
