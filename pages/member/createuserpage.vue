893338dcb0fdfd03ae5f9c6262599aa96af70121
amal1698 <amal1698@edu.ucl.dk>
2022-12-27 14:44:31 +0100

    option ++

--- pages/member/createuserpage.vue
+++ pages/member/createuserpage.vue
@@ -9,16 +9,15 @@
             <input type="email" autocomplete="on" name="email-input" placeholder="E-mail" required v-model="email" />
             <input type="password" minlength="10" name="password-input" placeholder="Adgangskode" required
                 v-model="password" />
-            <input type="file" name="memberImageTesting" v-on:change="memberImageTesting" />
-            <input type="file" name="memberPicture" v-on:change="memberPicture" />
+            <!-- <input type="file" name="memberPicture" v-on:change="memberPicture" /> -->
             <select id="options" name="memberEducationInstitution" required v-model="memberEducationInstitution">
-                <option value="hej" selected>Vælg din uddannelsesinstution</option>
-                <option value="Aalborg Universitet">Aalborg Universitet</option>
-                <option value="Aarhus Universitet">Aarhus Universitet</option>
-                <option value="Københavns Universitet, Nørre Campus">
+                <option disabled hidden selected>Vælg din uddannelsesinstution</option>
+                <option value="Aalborg Universitet" disabled>Aalborg Universitet</option>
+                <option value="Aarhus Universitet" disabled>Aarhus Universitet</option>
+                <option value="Københavns Universitet, Nørre Campus" disabled>
                     Københavns Universitet, Nørre Campus
                 </option>
-                <option value="Syddansk Universitet, Esbjerg">
+                <option value="Syddansk Universitet, Esbjerg" disabled>
                     Syddansk Universitet, Esbjerg
                 </option>
                 <option value="Syddansk Universitet, Odense">
@@ -43,8 +42,7 @@ const comments = ref("");
 const email = ref("");
 // const memberEducationInstitution = [""];
 const memberEducationInstitution = ["Syddansk Universitet, Odense"];
-const memberPicture = ref("");
-const memberImageTesting = ref["84ca7bf4-7e6b-43c2-be23-8d6e74a97895"];
+// const memberPicture = ref("");
 const name = ref("");
 const password = ref("");
 const username = ref("");
@@ -64,8 +62,7 @@ async function createMember() {
             isApproved: true,
             isLockedOut: false,
             memberEducationInstitution: memberEducationInstitution,
-            memberImageTesting: memberImageTesting,
-            memberPicture: memberPicture,
+            // memberPicture: memberPicture,
             memberTypeAlias: "Member",
             name: name,
             password: password,
